23579293a43196faf350ae10ca684a587d05ef18
M-hossameldine <m.hossameldine@gmail.com>
2025-10-04 03:49:47 +0300

    feature: handle pages lazy loading

--- src/core/AppLayout/index.tsx
+++ src/core/AppLayout/index.tsx
@@ -1,10 +1,23 @@
+import { Suspense } from 'react';
 import { Layout } from '@/components/Layout';
 import { AppRoutesProvider } from '../AppRoutesProvider';
+
+import { Loader } from 'lucide-react';
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
+};
 
 export const AppLayout = () => {
   return (
     <Layout>
-      <AppRoutesProvider />
+      <Suspense fallback={<Loading />}>
+        <AppRoutesProvider />
+      </Suspense>
     </Layout>
   );
 };
